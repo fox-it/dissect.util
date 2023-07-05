931f5ca52cd51d056ff1c84c86274bb42442dc11
Erik Schamper <1254028+Schamper@users.noreply.github.com>
2023-07-05 14:41:56 +0200

    Fix LZO decompression (#28)
    Co-authored-by: Miauwkeru <Miauwkeru@users.noreply.github.com>

--- dissect/util/compression/lzo.py
+++ dissect/util/compression/lzo.py
@@ -1,32 +1,25 @@
+# References:
+# - https://github.com/FFmpeg/FFmpeg/blob/master/libavutil/lzo.c
+# - https://docs.kernel.org/staging/lzo.html
+# - https://github.com/torvalds/linux/blob/master/lib/lzo/lzo1x_decompress_safe.c
+
 import io
 import struct
 from typing import BinaryIO, Union
 
+MAX_READ_LENGTH = (1 << 32) - 1000
 
-def _count_zeroes(src: BinaryIO):
-    length = 0
-    val = src.read(1)[0]
-    while val == 0:
+
+def _read_length(src: BinaryIO, val: int, mask: int) -> int:
+    if (length := val & mask) != 0:
+        return length
+
+    while (val := src.read(1)[0]) == 0:
+        if length >= MAX_READ_LENGTH:
+            raise ValueError("Invalid encoded length")
         length += 255
-        val = src.read(1)[0]
-        if length > 2**20:
-            raise ValueError("Too many zeroes")
 
-    return length + val
-
-
-def _copy_block(src: BinaryIO, dst: bytearray, length: int, distance: int, trailing: int):
-    remaining = length
-
-    block = dst[-distance:]
-    remaining -= len(block)
-    while remaining > 0:
-        add = block[:remaining]
-        remaining -= len(add)
-        block += add
-
-    dst.extend(block[:length])
-    dst.extend(src.read(trailing))
+    return length + mask + val
 
 
 def decompress(src: Union[bytes, BinaryIO], header: bool = True, buflen: int = -1) -> bytes:
@@ -56,56 +49,62 @@ def decompress(src: Union[bytes, BinaryIO], header: bool = True, buflen: int = -
         out_len = buflen
 
     val = src.read(1)[0]
-    if val == 0x10:
-        raise ValueError("LZOv1")
-    elif val >= 0x12:
-        dst += src.read(val - 0x11)
+    offset = src.tell()
+    if src.seek(5) == 5 and val == 17:
+        src.seek(offset)
+        _ = src.read(1)  # This would be the bitstream version but we don't currently use it
+        val = src.read(1)[0]
+    else:
+        src.seek(offset)
+
+    if val > 17:
+        dst += src.read(val - 17)
         val = src.read(1)[0]
 
-    trailing = 0
+        if val < 16:
+            raise ValueError("Invalid LZO stream")
+
+    state = 0
     while True:
-        if val <= 0xF:
-            if not trailing:
-                if val == 0:
-                    dst += src.read(_count_zeroes(src) + 18)
-                else:
-                    dst += src.read(val + 3)
+        if val > 15:
+            if val > 63:
+                # Copy 3-8 bytes from block within 2kb distance
+                length = (val >> 5) - 1
+                dist = (src.read(1)[0] << 3) + ((val >> 2) & 7) + 1
+            elif val > 31:
+                # Copy of small block within 16kb distance
+                length = _read_length(src, val, 31)
+                val = src.read(1)[0]
+                dist = (src.read(1)[0] << 6) + (val >> 2) + 1
             else:
-                h = src.read(1)[0]
-                dist = (h << 2) + (val >> 2) + 1
-                length = 2
-                trailing = val & 3
-                _copy_block(src, dst, length, dist, trailing)
-        elif val <= 0x1F:
-            if val & 7 == 0:
-                length = 9 + _count_zeroes(src)
-            else:
-                length = (val & 7) + 2
-            ds = struct.unpack("<H", src.read(2))[0]
-            dist = 16384 + ((val & 8) >> 3) + (ds >> 2)
-            if dist == 16384:
-                break
-            trailing = ds & 3
-            _copy_block(src, dst, length, dist, trailing)
-        elif val <= 0x3F:
-            length = val & 31
-            if length == 0:
-                length = _count_zeroes(src) + 31
-            length += 2
-            ds = struct.unpack("<H", src.read(2))[0]
-            dist = 1 + (ds >> 2)
-            trailing = ds & 3
-            _copy_block(src, dst, length, dist, trailing)
+                # Copy of a block within 16...48kB distance
+                length = _read_length(src, val, 7)
+                dist = (1 << 14) + ((val & 8) << 11)
+                val = src.read(1)[0]
+                dist += (src.read(1)[0] << 6) + (val >> 2)
+                if dist == (1 << 14):
+                    if length != 1:
+                        raise ValueError("Invalid LZO stream")
+                    break
+        elif not state:
+            # Copy 4 or more literals, depending on the last 4 bits
+            length = _read_length(src, val, 15)
+            dst += src.read(length + 3)
+            val = src.read(1)[0]
+            if val > 15:
+                continue
+            length = 1
+            dist = (1 << 11) + (src.read(1)[0] << 2) + (val >> 2) + 1
         else:
-            if val <= 0x7F:
-                length = 3 + ((val >> 5) & 1)
-            else:
-                length = 5 + ((val >> 5) & 3)
-            h = src.read(1)[0]
-            d = (val >> 2) & 7
-            dist = (h << 3) + d + 1
-            trailing = val & 3
-            _copy_block(src, dst, length, dist, trailing)
+            length = 0
+            dist = (src.read(1)[0] << 2) + (val >> 2) + 1
+
+        for _ in range(length + 2):
+            dst.append(dst[-dist])
+
+        # State is often encoded in the last 2 bits of the value, and used in subsequent iterations
+        state = length = val & 3
+        dst += src.read(length)
 
         if len(dst) == out_len:
             break
