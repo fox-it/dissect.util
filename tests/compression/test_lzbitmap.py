88e1b08b425a429cbc2107cb5befa3f06c5a5660
Schamper <1254028+Schamper@users.noreply.github.com>
2025-11-27 11:55:43 +0100

    Add snappy decompression

--- tests/compression/test_lzbitmap.py
+++ tests/compression/test_lzbitmap.py
@@ -11,7 +11,7 @@ if TYPE_CHECKING:
     from pytest_benchmark.fixture import BenchmarkFixture
 
 
-@pytest.mark.parametrize(
+PARAMS = (
     ("data", "digest"),
     [
         pytest.param(
@@ -49,41 +49,14 @@ if TYPE_CHECKING:
         ),
     ],
 )
+
+
+@pytest.mark.parametrize(*PARAMS)
 def test_lzbitmap_decompress(data: str, digest: str) -> None:
     assert hashlib.sha256(lzbitmap.decompress(bytes.fromhex(data))).hexdigest() == digest
 
 
 @pytest.mark.benchmark
-def test_benchmark_lzbitmap_decompress(benchmark: BenchmarkFixture) -> None:
-    buf = bytes.fromhex(
-        "5a424d092d0000a0000018000018000018000061616161616161617835ef"
-        "340f0000f10f00000000000000000000000000060000000000",
-    )
-    assert benchmark(lzbitmap.decompress, buf) == b"a" * 158 + b"xa"
-
-
-@pytest.mark.benchmark
-def test_benchmark_large_lzbitmap_decompress(benchmark: BenchmarkFixture) -> None:
-    buf = bytes.fromhex(
-        "5a424d09d80100df36002301005001007601004c6f72656d20697073756d"
-        "20646f6c6f722073742061657420636e7365636574757220616469706973"
-        "696e67656c69742e517573716566617563627520657820736170656e7661"
-        "6570656c6c6e6573656d6c61637261496e20696475727573206d69707269"
-        "74656c757364206f76616c7354656d7075206c656f7561656e6561646469"
-        "6d206e2074656d706f72506c6e7276697675736672696c61206c636e636d"
-        "657473626962656e64756d67657461732e496c696d6173206e6c6d616c75"
-        "64616c696e616e746567726e6e63707365722e5574206864726572697473"
-        "6d7072766c637373207074656e74747469736f6f73712e41646c696f7261"
-        "6f7175657465727562696f74726370746f686d6f732e0a0a4c0d2c341b41"
-        "3e26113c5f6e805b6b7c65529d967ec4b310edb922ca7b1deca5faf4434a"
-        "fbfa52fb8272b2ffb7016f7fcbc1b9f1373af99e3eb4e94fa9b3bafe39d3"
-        "6959add6f36b55eecdb59d2ec3d1d029fc0055a9cbed016111718114103f"
-        "480147110116011040f125f3ffffffffffffffffffffffffffffffffffff"
-        "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
-        "ffffffffffffffff1f002cf68f57d9c576d73bfd7cd3d756000006000000"
-        "0000",
-    )
-    assert (
-        hashlib.sha256(benchmark(lzbitmap.decompress, buf)).hexdigest()
-        == "73d3dd96ca2e2f0144a117019256d770ee7c6febeaee09b24956c723ae22b529"
-    )
+@pytest.mark.parametrize(*PARAMS)
+def test_benchmark_lzbitmap_decompress(data: str, digest: str, benchmark: BenchmarkFixture) -> None:
+    assert hashlib.sha256(benchmark(lzbitmap.decompress, bytes.fromhex(data))).hexdigest() == digest
